bcf09125480d39b82c6362c3b5f638c5f4832981
Alfonso Catanzaro <alfonso.catanzaro@gmail.com>
2021-11-27 18:42:09 +0100

    First commit

---
+++ Data/Entities/Product.cs
@@ -0,0 +1,9 @@
+namespace CORS.Sample.Data.Entities {
+  public class Product {
+    public int ProductId { get; set; }
+    public string Name { get; set; }
+    public string Manufacteur { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+  }
+}
---
+++ Data/MyDbContext.cs
@@ -0,0 +1,17 @@
+using CORS.Sample.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CORS.Sample.Data {
+  public class MyDbContext : DbContext {
+    public MyDbContext (DbContextOptions<MyDbContext> options) { }
+
+    public DbSet<Product> Products { get; set; }
+
+    protected override void OnModelCreating (ModelBuilder modelBuilder) {
+      modelBuilder.Entity<Product> (b => {
+        b.HasKey (_ => _.ProductId);
+        b.Property (_ => _.ProductId).ValueGeneratedOnAdd ();
+      });
+    }
+  }
+}
